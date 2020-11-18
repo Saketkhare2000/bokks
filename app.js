fc304a83657938f28505f260e4883c22112f1d00
Saketkhare2000 <saketkhare2000@gmail.com>
2020-11-18 07:45:16 +0530

    new

---
+++ app.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.svg-icon');
+
+// hamburger.addEventListener('click', ()=>{
+//     hamburger.classList.toggle('fa-bars')
+    // hamburger.classList.add('fa-times')
+// })
